e1a983ce2d616fc9d4eeb435f24dad8c6eaf5ea3
NIMAL M G <nimalmgaiml@gmail.com>
2026-03-09 10:46:26 +0530

    Time: 6 ms (2.19%) | Memory: 43.3 MB (82.46%) - LeetSync

---
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkTwoChessboards(String c1, String c2) {
+        int n1 = c1.charAt(0) - 'a' + 1;
+        int n2 = c2.charAt(0) - 'a' + 1;
+        int m1 = c1.charAt(1) - '0';
+        int m2 = c2.charAt(1) - '0';
+        int sum1 = n1+n2;
+        int sum2 = m1+m2;
+        System.out.println(sum1);
+        System.out.println(sum2);
+        if(sum1 % 2 == 0 && sum2 %2 == 0){
+            return true;
+        }
+        else if(sum1%2 != 0 && sum2 %2 != 0) {
+            return true;
+        }
+        return false;
+    }
+}
