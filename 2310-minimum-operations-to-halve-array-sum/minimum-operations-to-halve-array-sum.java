d7d32226338c1bac9f3d786e504a8c20caeae7b2
NIMAL M G <nimalmgaiml@gmail.com>
2026-03-14 11:02:39 +0530

    Time: 287 ms (68.57%) | Memory: 103.4 MB (94.03%) - LeetSync

---
+++ 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int halveArray(int[] nums) {
+        int cnt = 0;
+        PriorityQueue<Double> pq = new PriorityQueue<>(Collections.reverseOrder());
+        double sum = 0;
+        for(int n : nums){
+            sum += n;
+            pq.add((double)n);
+        }
+
+        sum = sum / 2;
+        int ans = 0;
+        while(sum > 0){
+            double t = pq.poll();
+            t = t/2;
+            pq.add(t);
+            sum -= t;
+            cnt++;
+        }
+        return cnt;
+    }
+}
