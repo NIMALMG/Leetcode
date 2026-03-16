8fcec653e0c47854d8a82863f180bb90fd9b12e4
NIMAL M G <nimalmgaiml@gmail.com>
2026-03-16 11:58:53 +0530

    Time: 2 ms (97.39%) | Memory: 46.5 MB (92.55%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder sb = new StringBuilder();
+        for(String s : words){
+            int sum = 0;
+            for(int i = 0;i<s.length();i++){
+                sum += weights[s.charAt(i) - 'a']; 
+            }
+            sb.append((char)('z' - sum%26));
+        }
+        return sb.toString();
+    }
+}
