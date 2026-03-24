5f71a90f1bcc3a6235da8d4cfc9e0fcfba0190a8
NIMAL M G <nimalmgaiml@gmail.com>
2026-03-25 00:23:42 +0530

    Time: 1 ms (99.84%) | Memory: 43.9 MB (38.00%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int cnt = 0;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(char ch : word.toCharArray()){
+            if(ch - 'a' >= 0){
+                freq1[ch - 'a']++;
+            }
+            else{
+                freq2[ch - 'A']++;
+            }
+        }
+
+        for(int i = 0;i<freq1.length;i++){
+            if(freq1[i]>0 && freq2[i]>0){
+                cnt++;
+            }
+        }
+        // System.out.println(Arrays.toString(freq1));
+        // System.out.println(Arrays.toString(freq2));
+        return cnt;
+    }
+}
