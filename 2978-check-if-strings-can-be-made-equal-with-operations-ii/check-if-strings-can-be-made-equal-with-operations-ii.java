a350f0dc84118621f8a47cece72cf408132f4bc1
NIMAL M G <nimalmgaiml@gmail.com>
2026-03-30 11:03:22 +0530

    Time: 6 ms (61.84%) | Memory: 48.1 MB (53.95%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,16 +1,30 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int[] freq = new int[52];
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
         for(int i = 0;i<s1.length();i++){
-            int of = (i&1) * 26;
-            freq[s1.charAt(i)-'a' + of]++;
-            freq[s2.charAt(i)-'a' + of]--;
+            freq1[s1.charAt(i)-'a']++;
         }
 
-        for(int i = 0;i<freq.length;i++){
-            if(freq[i]!=0){
-                return false;
-            }
+        for(int i = 0;i<s1.length();i++){
+            freq2[s2.charAt(i)-'a']++;
+        }
+
+        for(int i =0;i<26;i++){
+            if(freq1[i]!=freq2[i]) return false;
+            freq1[i]=0;
+            freq2[i]=0;
+        }
+        for(int i=1;i<s1.length();i+=2){
+            freq1[s1.charAt(i)-'a']++;
+        }
+        for(int i=1;i<s2.length();i+=2){
+            freq2[s2.charAt(i)-'a']++;
+        }
+    //    System.out.println(Arrays.toString(freq1));
+    //    System.out.print(Arrays.toString(freq2));
+        for(int i =0;i<26;i++){
+            if(freq1[i]!=freq2[i]) return false;
         }
         return true;
     }
