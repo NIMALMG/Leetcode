5d74568b177f8376f14ec69122150efcb6d08b43
NIMAL M G <nimalmgaiml@gmail.com>
2026-05-17 23:15:26 +0530

    Time: 3 ms (38.60%) | Memory: 42.7 MB (97.73%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] freq = new int[26];
+        for(int i = 0;i<t.length();i++){
+            freq[t.charAt(i) - 'a']++;
+        }
+        for(int j = 0;j<s.length();j++){
+            freq[s.charAt(j) - 'a']--;
+        }
+
+        for(int k = 0;k<freq.length;k++){
+            if(freq[k] == 1){
+                return (char)(k + 'a');
+            }
+        }
+        return ' ';
+    }
+}
