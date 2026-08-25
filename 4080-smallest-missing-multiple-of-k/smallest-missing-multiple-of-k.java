4fff242c7021fc1ea0243c8fa6b9853545209b1e
NIMAL M G <nimalmgaiml@gmail.com>
2026-08-25 14:51:05 +0530

    Time: 2 ms (68.20%) | Memory: 45.8 MB (6.83%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        for(int i = 1;i<=set.size()+1;i++){
+            if(!set.contains(i*k)){
+                return i*k;
+            }
+        }
+        return -1;
+    }
+}
